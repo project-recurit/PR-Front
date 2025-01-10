6e8c105cfd3683c438e40656ec60ccc2506c09be
doo1b <tngml928@gmail.com>
2025-01-10 16:02:14 +0900

    feat: 소셜 로그인 3종 구현

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Kakao from "next-auth/providers/kakao";
+
+const handler = NextAuth({
+  providers: [
+    Kakao({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
