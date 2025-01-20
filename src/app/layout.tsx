7e4b73fa895c2868803186c3d141ff2b8d5470aa
doo1b <tngml928@gmail.com>
2025-01-20 15:31:18 +0900

    [remove]: 불필요한 초기파일 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import AuthSession from "@/components/providers/SessionProvider";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,11 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko" className="trancy-ko">
       <AuthSession>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={` antialiased`}>{children}</body>
       </AuthSession>
     </html>
   );
