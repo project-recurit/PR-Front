5cb4288daafc09a0fb488217249e58813556f7ba
doo1b <tngml928@gmail.com>
2025-01-20 19:46:34 +0900

    [fix]: workflow 파일 수정

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const apiClient = axios.create({
+export const apiClient = axios.create({
   baseURL: process.env.API_BASE_URL,
-  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
