089c47e71d058f26b8e56b8fc2e50d631b01ac0b
이원빈 <ldcumg@naver.com>
2025-06-13 21:43:10 +0900

    update: 임시 middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import ROUTES from "@/constants/routes";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -7,16 +8,18 @@ export const middleware = async (request: NextRequest) => {
 
   const isLoggedIn = !!session?.user;
 
+  //TODO - 수정
   const isProtectedPath = request.nextUrl.pathname.startsWith("/protected");
 
   if (isProtectedPath && !isLoggedIn) {
-    const loginUrl = new URL("/api/auth/signin", request.url);
+    const loginUrl = new URL(ROUTES.logIn, request.url);
     return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
 };
 
+//TODO - 수정
 // 보호할 경로 설정
 export const config = {
   matcher: ["/protected/:path*"],
